689268b20e7b1a6dd4731910789f052d8d0ce4f5
Rane Gray <ranegray830@gmail.com>
2023-03-25 11:39:52 +0100

    Added unique prisma index

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -7,19 +7,38 @@ const router = Router();
 router.get('/application', (req, res) => {
     res.json({'message':'hello'})
 })
-router.get('/application/:id', () => {})
-router.put('/application/:id', body('jobTitle').isString(), (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+router.get('/application/:id', (req, res) => {
+
 })
-router.post('/application', (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+router.put('/application/:id', 
+    body('jobTitle').optional().isString(),
+    body('companyName').optional().isString(),
+    body('status').optional().isIn(['Saved', 'Applied', 'InterviewOffer', 'JobOffer', 'Rejected']),
+    body('contact').optional().isString(),
+    body('linkToApplication').optional().isURL(),
+    body('dateApplied').optional().isString(),
+    body('notes').optional().isString().isLength({max: 140}),
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+    })
+router.post('/application', 
+    body('jobTitle').isString(),
+    body('companyName').isString(),
+    body('contact').optional().isString(),
+    body('linkToApplication').optional().isURL(),
+    body('dateApplied').optional().isString(),
+    body('notes').optional().isString().isLength({max: 140}),
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+        }
+    })
+router.delete('/application/:id', (req, res) => {
+    
 })
-router.delete('/application/:id', () => {})
 
 export default router;
