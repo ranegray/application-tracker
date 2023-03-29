7a57a4510b11094dbc6d6e66a83e517ea2eb01a5
Rane Gray <ranegray830@gmail.com>
2023-03-29 14:27:10 +0200

    Refactoring to custom API away from Firebase and better frontend validation

--- frontend/src/Components/Form.jsx
+++ frontend/src/Components/Form.jsx
@@ -1,27 +1,49 @@
+import { useState } from "react";
 import { db } from "../firebase.js";
 import { collection, addDoc } from "firebase/firestore";
 
 export default function Form({ getApplications, setShowModal }) {
-  const saveApplication = (event) => {
-    setShowModal(false);
-    const form = document.querySelector("form");
-    event.preventDefault();
+  const [formData, setFormData] = useState({
+    companyName: "",
+    jobTitle: "",
+    status: "",
+    linkToApplication: "",
+    dateApplied: "",
+    contact: "",
+    resume: "",
+    notes: "",
+  });
 
-    const elementsArray = [...event.target.elements];
-
-    const formData = elementsArray.reduce((acc, curr) => {
-      if (curr.id) {
-        acc[curr.id] = curr.value;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    setFormData(() => {
+      for (let i = 0; i < 8; i++) {
+        formData[e.target[i].name] = e.target[i].value;
       }
-      return acc;
-    }, {});
-    form.reset();
-    const createApplication = async (form) => {
-      await addDoc(collection(db, "applications"), form);
-    };
-    createApplication(formData);
-    getApplications();
+    });
   };
+
+  // const saveApplication = (event) => {
+  //   setShowModal(false);
+  //   const form = document.querySelector("form");
+  //   event.preventDefault();
+
+  //   const elementsArray = [...event.target.elements];
+
+  //   const formData = elementsArray.reduce((acc, curr) => {
+  //     if (curr.id) {
+  //       acc[curr.id] = curr.value;
+  //     }
+  //     return acc;
+  //   }, {});
+  //   form.reset();
+  //   const createApplication = async (form) => {
+  //     await addDoc(collection(db, "applications"), form);
+  //   };
+  //   createApplication(formData);
+  //   getApplications();
+  // };
 
   return (
     <div
@@ -34,16 +56,17 @@ export default function Form({ getApplications, setShowModal }) {
         }}
         className="w-80 shadow-md px-8 rounded pt-6 pb-8 mb-4 bg-white"
       >
-        <form onSubmit={saveApplication}>
+        <form onSubmit={handleSubmit}>
           <div>
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="company"
+              htmlFor="companyName"
             >
               Company
               <input
                 type="text"
-                id="company"
+                id="companyName"
+                name="companyName"
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 required
               />
@@ -52,46 +75,49 @@ export default function Form({ getApplications, setShowModal }) {
           <div>
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="role"
+              htmlFor="jobTitle"
             >
-              Role
+              Job Title
               <input
                 type="text"
-                id="role"
+                id="jobTitle"
+                name="jobTitle"
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 required
               />
             </label>
           </div>
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="Status"
-          >
-            Status
-            <select
-              type="text"
-              id="company"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
-            >
-              <option value="Saved" selected>
-                Saved
-              </option>
-              <option value="Applied">Applied</option>
-              <option value="InterviewOffer">Interview Offer</option>
-              <option value="JobOffer">Job Offer</option>
-              <option value="Rejected">Rejected</option>
-            </select>
-          </label>
           <div>
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="source"
+              htmlFor="status"
+            >
+              Status
+              <select
+                type="text"
+                id="status"
+                name="status"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                required
+              >
+                <option value="Saved">Saved</option>
+                <option value="Applied">Applied</option>
+                <option value="InterviewOffer">Interview Offer</option>
+                <option value="JobOffer">Job Offer</option>
+                <option value="Rejected">Rejected</option>
+              </select>
+            </label>
+          </div>
+          <div>
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="linkToApplication"
             >
               Application Link
               <input
                 type="text"
-                id="source"
+                id="linkToApplication"
+                name="linkToApplication"
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 required
               />
@@ -100,12 +126,13 @@ export default function Form({ getApplications, setShowModal }) {
           <div>
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="date-applied"
+              htmlFor="dateApplied"
             >
               Date Applied
               <input
                 type="date"
-                id="date-applied"
+                id="dateApplied"
+                name="dateApplied"
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               />
             </label>
@@ -119,6 +146,7 @@ export default function Form({ getApplications, setShowModal }) {
               <input
                 type="text"
                 id="contact"
+                name="contact"
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               />
             </label>
@@ -132,6 +160,8 @@ export default function Form({ getApplications, setShowModal }) {
               <input
                 type="file"
                 id="resume"
+                name="resume"
+                disabled
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               />
             </label>
@@ -144,13 +174,17 @@ export default function Form({ getApplications, setShowModal }) {
               Notes (optional)
               <textarea
                 id="notes"
+                name="notes"
                 maxLength={140}
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               />
             </label>
           </div>
           <div className="flex justify-center">
-            <button className="w-20 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+            <button
+              type="submit"
+              className="w-20 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+            >
               Track
             </button>
           </div>
